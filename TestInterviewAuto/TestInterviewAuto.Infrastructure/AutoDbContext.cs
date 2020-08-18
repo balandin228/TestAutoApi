12ecb65e6c0c6c2a553b883bfec4062a62c14189
balandin228 <igorek.chelovek@gmail.com>
2020-08-18 22:31:16 +0500

    Crate DbContext

---
+++ TestInterviewAuto/TestInterviewAuto.Infrastructure/AutoDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestInterviewAuto.Domain.Model.Car;
+using TestInterviewAuto.Domain.Model.CarBrand;
+using TestInterviewAuto.Domain.Model.Color;
+
+namespace TestInterviewAuto.Infrastructure
+{
+    public class AutoDbContext : DbContext
+    {
+        public DbSet<Car> Cars;
+
+        public DbSet<CarBrand> CarBrands;
+        public DbSet<CarColor> CarColors;
+
+        public AutoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AutoDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
