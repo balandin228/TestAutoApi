4581392eb080f0c62860caac02f3c26c29ee7362
balandin228 <igorek.chelovek@gmail.com>
2020-08-18 22:51:12 +0500

    Add carRepository

---
+++ TestInterviewAuto/TestInterviewAuto.Infrastructure/Repositories/CarRepository/CarRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TestInterviewAuto.Domain.Model.Car;
+
+namespace TestInterviewAuto.Infrastructure.Repositories.CarRepository
+{
+    public class CarRepository : EFRepository<Car>, ICarRepository
+    {
+        public CarRepository(AutoDbContext context) : base(context)
+        {
+        }
+
+        protected override IQueryable<Car> Items => base.Items
+            .Include(c => c.Color)
+            .Include(c => c.CarBrand);
+    }
+}
---
+++ TestInterviewAuto/TestInterviewAuto.Infrastructure/Repositories/CarRepository/ICarRepository.cs
@@ -0,0 +1,9 @@
+﻿using TestInterviewAuto.Domain.Model.Car;
+
+namespace TestInterviewAuto.Infrastructure.Repositories.CarRepository
+{
+    public interface ICarRepository: IRepository<Car>
+    {
+        
+    }
+}
