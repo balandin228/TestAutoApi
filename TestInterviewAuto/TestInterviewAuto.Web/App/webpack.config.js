58f8d61881dea4c10d3827c1924367a285dbd989
Balandin228 <igorek.chelovek@gmail.com>
2020-09-05 18:58:09 +0500

    add babel

--- TestInterviewAuto/TestInterviewAuto.Web/App/webpack.config.js
+++ TestInterviewAuto/TestInterviewAuto.Web/App/webpack.config.js
@@ -2,8 +2,23 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: "source-map",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
   },
+  module: {
+    rules: [
+        {
+            test: /\.jsx$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ["es2015", "stage-0", "react"]
+                }
+              }
+        }
+    ]
+},
 };
